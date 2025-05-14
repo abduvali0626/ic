201cb022b092e86d250c65bbec6ef98b5701b971
abduvali0626 <umarrrrrrrrrrrrrrr63@gmail.com>
2025-05-14 17:40:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let div = document.getElementById("p")
+
+function btn1() {
+    document.body.style.backgroundColor = "black"
+    document.body.style.color = "white"
+}
+
+function btn2() {
+    document.body.style.backgroundColor = "white"
+    document.body.style.color = "black"
+}
+
+function btn3() {
+  p.textContent = "Asia (/ˈeɪʒə/ ⓘ AY-zhə, UK also /ˈeɪʃə/ AY-shə) is the largest continent[note 1][10][11] in the world by both land area and population.[11] It covers an area of more than 44 million square kilometres,[note 2] about 30% of Earth's total land area and 8% of Earth's total surface area. The continent, which has long been home to the majority of the human population,[12] was the site of many of the first civilisations. Its 4.7 billion people constitute roughly 60% of the world's population."
+}
